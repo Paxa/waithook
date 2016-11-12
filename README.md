b4ac02db04ed52060d60518ae0c202d526435f6b
Pavel <pavel.evst@gmail.com>
2016-11-12 15:44:14 +0800

    Add README, fix link in footer

---
+++ README.md
@@ -0,0 +1,46 @@
+# Waithook - HTTP to web-socket transmitter
+
+An excelent tool to debug incoming HTTP notifications. Let's say your application need to receive HTTP request from 3rd parties.
+It can payment provider or incoming email processing or GitHub push notifications, etc.
+Developing it on localhost can be hard, using SSH tunnels always gave me hard times.
+
+## How it Works
+
+1. Open web-socket connection to https://waithook.herokuapp.com/something (choose your namespace)
+2. Send HTTP request to https://waithook.herokuapp.com/something
+3. Waithook will forward it as json to all listening web-socket connections
+
+Example of message send to websocket:
+```json
+{
+  "method": "POST",
+  "url": "/testing_346?foo=bar",
+  "headers": {
+    "X-Request-Start": "1478935535042",
+    "X-Forwarded-For": "182.253.140.80",
+    "Total-Route-Time": "0",
+    "Accept": "*/*",
+    "Connection": "close",
+    "Via": "1.1 vegur",
+    "Content-Type": "application/json",
+    "Content-Length": "69",
+    "Connect-Time": "1",
+    "Origin": "https://waithook.herokuapp.com",
+    "Referer": "https://waithook.herokuapp.com/",
+    "Host": "waithook.herokuapp.com",
+    "X-Request-Id": "5d12e9b2-31d1-47b1-abc9-f9bca03dc4bf",
+    "X-Forwarded-Proto": "https",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.8",
+    "X-Forwarded-Port": "443",
+    "Accept-Encoding": "gzip, deflate, br"
+  },
+  "body": "{\n  \"type\": \"Testing Request\",\n  \"time\": \"2016-11-12T07:25:34.280Z\"\n}"
+}
+```
+
+## Development
+
+```
+cargo run
+```
---
+++ test_client/reply_client.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+var WebSocketClient = require('websocket').client;
+var request = require('request');
+
+if (process.argv[2] === undefined || process.argv[3] === undefined) {
+  throw "required 2 command line arguments";
+}
+
+var LISTEN_TO = process.argv[2];
+
+var SEND_TO = process.argv[3];
+
+if (!SEND_TO.match(/^https?:\/\//)) {
+  SEND_TO = "http://" + SEND_TO;
+}
+
+console.log("Subscribing for 'wss://waithook.herokuapp.com/%s", LISTEN_TO);
+console.log("Forward to ", SEND_TO);
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+ 
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+            if (message.utf8Data[0] == "{") {
+              var data = JSON.parse(message.utf8Data);
+
+              var url = SEND_TO;
+              if (data.url.includes('?')) {
+                url += data.url.substring(data.url.indexOf('?'), data.url.length - 1);
+              }
+
+              request({
+                method: data.method,
+                url: url,
+                headers: data.headers,
+                body: data.body
+              }, function (error, response, body) {
+                if (error) {
+                  console.error("HTTP Error: ", error);
+                } else {
+                  console.log("HTTP Server Response: %s\n%s", response.statusCode, body);
+                }
+              });
+            }
+        }
+    });
+});
+
+//client.connect('ws://localhost:3012/' + LISTEN_TO);
+client.connect('wss://waithook.herokuapp.com/' + LISTEN_TO);
+
